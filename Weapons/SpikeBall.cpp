64112bfa7a94043e26fbd7280bd9571b227c175b
Chris <cbrenton@github.com>
2011-03-12 09:29:34 +0000

    Non-working spike ball code, AsteroidSurface image.
    git-svn-id: https://wiki.csc.calpoly.edu/svn/AsteroidBlaster@403 342171fe-4ba3-422f-b357-09b6f24955fe

---
+++ Weapons/SpikeBall.cpp
@@ -0,0 +1,123 @@
+/**
+ * Blaster! The original Asteroids Weapon.
+ * @author Sterling Hirsh (shirsh@calpoly.edu)
+ * @date 2/14/2011
+ * <3
+ */
+
+#include "Weapons/Blaster.h"
+#include "Utility/GlobalUtility.h"
+#include "Shots/ProjectileShot.h"
+#include "Utility/Point3D.h"
+#include "Utility/SoundEffect.h"
+
+Blaster::Blaster(AsteroidShip* owner)
+: Weapon(owner) {
+   shotSpeed = 40; // Units per second
+   coolDown = 0.15; // Seconds
+   randomVariationAmount = 1.5; // Units
+   name = "Blaster";
+   lastShotPos = new Point3D(0, 1, 0);
+   curAmmo = -1; // Infinite ammo
+   purchased = true; // Start off owning the blaster
+}
+
+Blaster::~Blaster() {
+   delete lastShotPos;
+}
+
+/**
+ * Called every frame.
+ * We'll probably keep track of something or other here.
+ */
+void Blaster::update(double timeDiff) {
+   // Do noting, yet
+}
+
+/**
+ * This is what actually shoots. Finally!
+ */
+void Blaster::fire() {
+   static Vector3D randomVariation;
+   if (!isCooledDown() && !gameState->godMode)
+      return;
+   // Update timeLastFired with new current time.
+   timeLastFired = doubleTime();
+   // Copy the ship's position for the start point.
+   Point3D start = *ship->position;
+   // Copy the shot direction, set length to shotSpeed (since shotDirection is unit-length).
+   Vector3D shotDirection(ship->shotDirection.scalarMultiply(shotSpeed));
+   // Add a random variation to each of the shots.
+   randomVariation.randomMagnitude();
+   randomVariation.scalarMultiplyUpdate(randomVariationAmount);
+   shotDirection.addUpdate(randomVariation);
+   ship->shotDirection.movePoint(start);
+   gameState->custodian.add(new ProjectileShot(start,
+            shotDirection, ship));
+   // Don't play sound effects in godMode b/c there would be too many.
+   if (!gameState->godMode) {
+      SoundEffect::playSoundEffect(1);
+   }
+}
+
+/**
+ * Basic debug function. Just in case!
+ */
+void Blaster::debug() {
+   printf("Blaster!\n");
+}
+
+/**
+ * The job of the weapon is to project the position of
+ * the targeted object, and return the point that the
+ * AI should aim at in order to hit the target with this
+ * weapon.
+ */
+Point3D Blaster::project(Object3D* target) {
+   Point3D wouldHit;
+   double speed = 40;
+   double time = 0, dist = 0;
+   int iterations = 0;
+
+   Point3D targetPos = *target->position;
+   Point3D curTarget = targetPos;
+   Point3D dp;
+
+   // This loop will choose the spot that we want to be shooting at.
+   do {
+      // time is the distance from the ship to the target according to the
+      // speed of the bullet.
+      time = ship->position->distanceFrom(curTarget) / speed;
+
+      // dp is the distance the asteroid traveled in the time it took for our
+      // bullet to get to the point we are considering (curTarget).
+      dp = target->velocity->scalarMultiply(time);
+
+      // Move our target to the point where the asteroid would be
+      curTarget = dp + targetPos;
+
+      // Get the vector that points to that point.
+      wouldHit = curTarget - *ship->position;
+
+      // Normalize, scale by speed of the bullet multiplied by the time that
+      // passed (for the bullet to get to the previous point) This vector
+      // now points to where our bullet will be when the asteroid is at
+      // its position
+
+      wouldHit = wouldHit.normalize() * speed * time + *ship->position;
+
+      // Dist is the distance from where our bullet will be to where
+      // the asteroid will be.
+      dist = wouldHit.distanceFrom(curTarget);
+      iterations++;
+      // If this distance is greater than the radius (ie, we missed),
+      // recalculate!
+   } while (dist > target->radius && iterations < 5);
+
+   return curTarget;
+
+}
+
+bool Blaster::shouldFire(Point3D* target, Point3D* aim) {
+   return ((*target - *ship->position).normalize() - *aim).magnitude() < 0.5;
+}
---
+++ Weapons/SpikeBall.h
@@ -0,0 +1,33 @@
+/**
+ * Blaster! The original Asteroids Weapon.
+ * @author Sterling Hirsh (shirsh@calpoly.edu)
+ * @date 2/14/2011
+ * <3
+ */
+
+#ifndef __BLASTER_H__
+#define __BLASTER_H__
+
+#include "Weapons/Weapon.h"
+
+struct Point3D;
+/* Classter Blaster */
+class Blaster : public Weapon {
+   public:
+      Blaster(AsteroidShip* owner);
+      virtual ~Blaster();
+      virtual Point3D project(Object3D*);
+      virtual void update(double timeDiff);
+      virtual bool shouldFire(Point3D*, Point3D*);
+      void godMode(bool enabled);
+      virtual void debug();
+      virtual void fire();
+
+   protected:
+      double shotSpeed;
+      double turnSpeed;
+      double randomVariationAmount;
+      Point3D* lastShotPos;
+};
+
+#endif
