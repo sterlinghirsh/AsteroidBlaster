5bfd750b41332e3494b679edd77a6ed12eb99a28
Ryuho <Ryuho@github.com>
2011-05-01 06:50:06 +0000

    added SDL_net
    git-svn-id: https://wiki.csc.calpoly.edu/svn/AsteroidBlaster@704 342171fe-4ba3-422f-b357-09b6f24955fe

--- lib.sh
+++ lib.sh
@@ -14,6 +14,7 @@ then
    tar -xf SDL_ttf-2.0.10.tar
    #deleting gunziped file
    rm SDL_ttf-2.0.10.tar
+   svn revert SDL_ttf-2.0.10.tar.gz
    cd SDL_ttf-2.0.10
    echo "Configuring.."
    ./configure || exit
@@ -30,6 +31,7 @@ then
    tar -xf SDL_mixer-1.2.11.tar
    #deleting gunziped file
    rm SDL_mixer-1.2.11.tar
+   svn revert SDL_mixer-1.2.11.tar.gz
    cd SDL_mixer-1.2.11
    echo "Configuring.."
    ./configure || exit
@@ -46,7 +48,25 @@ then
    tar -xf SDL_image-1.2.10.tar
    #deleting gunziped file
    rm SDL_image-1.2.10.tar
+   svn revert SDL_image-1.2.10.tar.gz
    cd SDL_image-1.2.10
+   echo "Configuring.."
+   ./configure || exit
+   echo "Making.."
+   make -j 3 || exit
+   echo "Done!"
+   cd ..
+   
+   #SDL_net--------------------
+   echo "SDL_net"
+   echo "Unzipping.."
+   gunzip SDL_net-1.2.7.tar.gz
+   echo "Extracting.."
+   tar -xf SDL_net-1.2.7.tar
+   #deleting gunziped file
+   rm SDL_net-1.2.7.tar
+   svn revert SDL_net-1.2.7.tar.gz
+   cd SDL_net-1.2.7
    echo "Configuring.."
    ./configure || exit
    echo "Making.."
@@ -62,39 +82,37 @@ then
    tar -xf glew-1.5.8.tar
    #deleting gunziped file
    rm glew-1.5.8.tar
+   svn revert glew-1.5.8.tar.gz
    cd glew-1.5.8
    echo "Making.."
    make -j 3 || exit
    echo "Done!"
    cd ..
    
-   #go back to parent directory
-   cd ..
-   svn revert -R Libraries
-   
 elif [ "$1" == "clean" ] 
 then
    cd Libraries
-
+   
    #SDL_ttf--------------------
-   echo "SDL_ttf"
-   echo "Cleaning..."
+   echo "Cleaning SDL_ttf..."
    rm -rf SDL_ttf-2.0.10
    
    #SDL_mixer--------------------
-   echo "SDL_ttf"
-   echo "Cleaning..."
+   echo "Cleaning SDL_mixer..."
    rm -rf SDL_mixer-1.2.11
    
    #SDL_image--------------------
-   echo "SDL_ttf"
-   echo "Cleaning..."
+   echo "Cleaning SDL_image..."
    rm -rf SDL_image-1.2.10
    
+   #SDL_net--------------------
+   echo "Cleaning SDL_net..."
+   rm -rf SDL_net-1.2.7
+   
    #glew--------------------
-   echo "GLEW"
-   echo "Cleaning..."
+   echo "Cleaning GLEW..."
    rm -rf glew-1.5.8
+   svn revert * 
 else
    echo "Usage: lib [make | clean]"
 fi
